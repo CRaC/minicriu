c6212e0c9519b1a2d21030a07ca6d6b0faf5ff07
Anton Kozlov <akozlov@azul.com>
2022-06-16 12:21:41 +0300

    Make minicriu-client.h c++ compatible

--- minicriu-client.h
+++ minicriu-client.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern int minicriu_register_new_thread(void);
 
 extern int minicriu_dump(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
